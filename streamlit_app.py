de17f94cc18eff421f917590b517ac05fcd4f5a2
nazysharif-sketch <nazysharif@hotmail.com>
2026-06-27 20:11:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ if st.button("🚀 GENERATE MUSIC VIDEO"):
                         style="
                             width: 100%;
                             height: 100%;
-                            object-fit: contain;
+                            object-fit: cover;
                             background: #111;
                         "></video>
                     </div>
